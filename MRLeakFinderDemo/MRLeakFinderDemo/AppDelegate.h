ab79f8e55c504d8a0d7ed65f3dae5728f77c691a
Roy <>
2019-02-23 19:00:23 +0800

    init

---
+++ MRLeakFinderDemo/MRLeakFinderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MRLeakFinderDemo
+//
+//  Created by Roy on 2019/2/23.
+//  Copyright © 2019年 Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MRLeakFinderDemo/MRLeakFinderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRLeakFinderDemo
+//
+//  Created by Roy on 2019/2/23.
+//  Copyright © 2019年 Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
